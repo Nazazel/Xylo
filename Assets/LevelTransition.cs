0db9a5611e5e173eec6cae8a1fa253d13227c546
CodiakVII <codiak759@gmail.com>
2016-11-02 17:40:42 -0700

    Added Almost Complete Level Transition and Elevator Code

---
+++ Assets/LevelTransition.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour {
+
+	bool loading = false;
+	public Image FadeImg;
+	public float fadeSpeed = 1.5f;
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (loading == false) 
+		{
+			loading = true;
+			InvokeRepeating ("FadeToBlack",0.0f, 0.02f);
+			StartCoroutine ("LoadElevator");
+		}
+	}
+
+	IEnumerator LoadElevator()
+	{
+		yield return new WaitForSeconds (5.0f);
+		//SceneManager.LoadSceneAsync ("Elevator");
+	}
+
+	void FadeToBlack()
+	{
+		FadeImg.color = Color.Lerp (FadeImg.color, Color.black, fadeSpeed * Time.deltaTime);
+		if (FadeImg.color.a == 1.0f) {
+			CancelInvoke ("FadeToBlack");
+		}
+	}
+}
