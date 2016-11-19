b71674f0ad953873da92daa345efa14988bb0aeb
TheAvianLord <angela-yixuan21@hotmail.com>
2016-11-18 17:13:19 -0800

    10 / 10 DOESNT EQUAL TO 1 APPARENTLY
    Added repair bar and timing to Comms Center

---
+++ Assets/Scripts/ActivateRepair.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ActivateRepair : MonoBehaviour
+{
+
+    public bool requireButtonPress;
+    private bool waitForPress;
+
+    public bool destroyWhenActivated;
+
+    public Text pickupText;
+    public GameObject player;
+    public GameObject repairBar;
+    public bool isDown;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        pickupText = GameObject.Find("ManualPickup").GetComponent<Text>();
+        pickupText.text = "";
+        player = GameObject.FindWithTag("Player");
+        repairBar = GameObject.FindWithTag("RepairBar");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waitForPress && Input.GetKey(KeyCode.R) && !isDown)
+        {
+            repairBar.GetComponent<Timer2>().started = true;
+            isDown = true;
+
+            if (destroyWhenActivated)
+            {
+                Destroy(gameObject);
+            }
+
+          
+        }
+
+        else
+        {
+            repairBar.GetComponent<Timer2>().started = false;
+            isDown = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+            pickupText.text = "Hold 'R' to repair";
+
+        if (other.name == "Stella")
+        {
+            if (requireButtonPress)
+            {
+                waitForPress = true;
+                return;
+            }
+
+
+            if (destroyWhenActivated)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+
+
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        pickupText.text = "";
+        if (other.name == "Stella")
+        {
+            waitForPress = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Timer2.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class Timer2 : MonoBehaviour
+{
+    Image fillImg;
+    public Text repairText;
+    float timeAmt = 10;
+    float time;
+    public bool started;
+    public bool isDone;
+    // Use this for initialization
+    void Start()
+    {
+
+        fillImg = this.GetComponent<Image>();
+        time = timeAmt;
+        started = false;
+        repairText.enabled = false;
+        fillImg.enabled = false;
+        isDone = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (started)
+        {
+            if (time > 0)
+            {
+                repairText.enabled = true;
+                fillImg.enabled = true;
+                time -= Time.deltaTime;
+                fillImg.fillAmount = (timeAmt - time) / timeAmt;
+                //Debug.Log((timeAmt - time) / timeAmt);
+
+
+                if (((timeAmt - time) / timeAmt) > 1)
+                {
+                    fillImg.color = new Color(0, 0, 0, 0);
+                    repairText.color = new Color(0, 0, 0, 0);
+                    isDone = true;
+                }
+            }
+
+        }
+    }
+}
+
