185bd7bba28483a41822c916d97980cb74e23b4a
InvalidCS <cjdipalma@yahoo.com>
2016-11-22 20:44:00 -0800

    Spawn modifications and deleted key cards

--- Assets/Scripts/Spawnpoint.cs
+++ Assets/Scripts/Spawnpoint.cs
@@ -9,18 +9,6 @@ public class Spawnpoint : MonoBehaviour {
 	void Start () {
         gameObject.GetComponent<MeshRenderer>().enabled = false;
         pl = GameObject.FindWithTag("Player");
-        if ((gameObject.scene.name.Equals("Level One") || gameObject.scene.buildIndex == 0) && pl.GetComponent<PlayerController>().firstSpawnInLV1 == true)
-        {
-            pl.SendMessage("tp", gameObject.transform.position);
-            pl.GetComponent<PlayerController>().firstSpawnInLV1 = false;
-        }
-        else if(gameObject.scene.buildIndex != 5 && gameObject.scene.buildIndex != 0)
-        {
-            pl.SendMessage("tp", gameObject.transform.position);
-        }
-        else if(gameObject.scene.buildIndex == 5 || gameObject.scene.buildIndex == 0)
-        {
-            pl.SendMessage("tpL1");
-        }
+        pl.SendMessage("tp", gameObject.transform.position);
     }
 }
