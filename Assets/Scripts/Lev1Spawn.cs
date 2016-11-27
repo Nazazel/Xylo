27d865f650fa52b100df97662276fffe193bad34
InvalidCS <cjdipalma@yahoo.com>
2016-11-26 17:00:43 -0800

    Level one clean spawning points

---
+++ Assets/Scripts/Lev1Spawn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lev1Spawn : MonoBehaviour
+{
+
+    private GameObject pl;
+    public int spawnId;
+    private bool active = false;
+    private int obj;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        obj = pl.GetComponent<PlayerController>().currentObjective;
+        pl = GameObject.FindWithTag("Player");
+        if (spawnId == 1 && (obj == 4 || obj == 10))
+            active = true;
+        else if (spawnId == 2 && obj == 5)
+            active = true;
+        else if (spawnId == 3 && obj == 8)
+            active = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
