fb40b2a3b0477a191ecda9dc47e6f88716ec71e5
InvalidCS <cjdipalma@yahoo.com>
2016-11-30 21:11:16 -0800

    Fixed level 1 spawn code

--- Assets/Scripts/Lev1Spawn.cs
+++ Assets/Scripts/Lev1Spawn.cs
@@ -13,16 +13,20 @@ public class Lev1Spawn : MonoBehaviour
     void Start()
     {
         gameObject.GetComponent<MeshRenderer>().enabled = false;
-        obj = pl.GetComponent<PlayerController>().currentObjective;
         pl = GameObject.FindWithTag("Player");
+        obj = pl.GetComponent<PlayerController>().currentObjective;
+        Debug.Log("Found spawn "+spawnId);
         if (spawnId == 1 && (obj == 4 ))
             active = true;
         else if (spawnId == 2 && (obj == 5 ))
             active = true;
         else if (spawnId == 3 && (obj >= 8 && obj <= 10))
             active = true;
-        if(active)
+        if (active)
+        {
+            Debug.Log("Spawn " + spawnId + " is active");
             pl.SendMessage("tp", gameObject.transform.position);
+        }
     }
 
     // Update is called once per frame
