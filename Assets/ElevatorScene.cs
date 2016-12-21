ac00c800446abb6c4f7a7a5a78c9d3cc2c06e569
CodiakVII <codiak759@gmail.com>
2016-12-21 13:35:19 -0800

    pls dont kill me. 92 problems

--- Assets/ElevatorScene.cs
+++ Assets/ElevatorScene.cs
@@ -12,17 +12,7 @@ public class ElevatorScene : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindWithTag ("Player");
-		InvokeRepeating ("FadeToClear", 1.0f, 0.1f);
 		StartCoroutine ("transitionToLevel");
-	}
-
-	void FadeToClear()
-	{
-		FadeImg.color = Color.Lerp (FadeImg.color, Color.clear, fadeSpeed * Time.deltaTime);
-		if (FadeImg.color.a < 0.05f) {
-			CancelInvoke ("FadeToClear");
-			FadeImg.color = Color.clear;
-		}
 	}
 
 	void FadeToBlack()
@@ -35,7 +25,7 @@ public class ElevatorScene : MonoBehaviour {
 
 	public IEnumerator transitionToLevel()
 	{
-		yield return new WaitForSeconds (10.0f);
+		yield return new WaitForSeconds (4.0f);
 		if (player.GetComponent<PlayerController> ().currentObjective == 5) {
 			InvokeRepeating ("FadeToBlack", 1.0f, 0.1f);
 			yield return new WaitForSeconds (3.0f);
