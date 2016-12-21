fa5c8bb3c818e89ce8c6aa5093cee425e56ad4b3
CodiakVII <codiak759@gmail.com>
2016-12-21 13:25:14 -0800

    93 problems...

--- Assets/ElevatorScene.cs
+++ Assets/ElevatorScene.cs
@@ -38,22 +38,22 @@ public class ElevatorScene : MonoBehaviour {
 		yield return new WaitForSeconds (10.0f);
 		if (player.GetComponent<PlayerController> ().currentObjective == 5) {
 			InvokeRepeating ("FadeToBlack", 1.0f, 0.1f);
-			yield return new WaitUntil (() => !IsInvoking ());
+			yield return new WaitForSeconds (3.0f);
 			SceneManager.LoadSceneAsync("Engineering Wing");
 		} 
 		else if (player.GetComponent<PlayerController> ().currentObjective == 8) {
 			InvokeRepeating ("FadeToBlack", 1.0f, 0.1f);
-			yield return new WaitUntil (() => !IsInvoking ());
+			yield return new WaitForSeconds (3.0f);
 			SceneManager.LoadSceneAsync("Medical Ward");
 		}
 		else if (player.GetComponent<PlayerController> ().currentObjective == 9) {
 			InvokeRepeating ("FadeToBlack", 1.0f, 0.1f);
-			yield return new WaitUntil (() => !IsInvoking ());
+			yield return new WaitForSeconds (3.0f);
 			SceneManager.LoadSceneAsync("Engine Room");
 		}
 		else if (player.GetComponent<PlayerController> ().currentObjective == 10) {
 			InvokeRepeating ("FadeToBlack", 1.0f, 0.1f);
-			yield return new WaitUntil (() => !IsInvoking ());
+			yield return new WaitForSeconds (3.0f);
 			SceneManager.LoadSceneAsync("Level One Clean");
 		}
 	}
