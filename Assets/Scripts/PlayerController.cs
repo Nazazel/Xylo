04e7d69592994f54b2413da4c4c9260f24c73ca1
CodiakVII <codiak759@gmail.com>
2017-01-04 12:50:08 -0800

    longer sequence

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1108,7 +1108,7 @@ public class PlayerController : MonoBehaviour {
 
 	public IEnumerator AlarmOffSequence()
 	{
-		yield return new WaitForSeconds (3.0f);
+		yield return new WaitForSeconds (6.0f);
 		CancelInvoke ("alarmOn");
 		if(AlarmUI != null)
 		{
